1cd3fdde2b7e5dda930d994debb304e0268eda63
NoahPG88 <57874889+NoahPG88@users.noreply.github.com>
2020-06-28 15:51:45 -0500

    trying to test

--- src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
@@ -5,8 +5,10 @@ import org.launchcode.codingevents.models.EventCategory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
@@ -31,9 +33,22 @@ public class EventCategoryController {
         return "eventCategories/index";
     }
 
-    @GetMapping
-    public String renderCreateEventCategoryForm(@ModelAttribute EventCategory newEventCategory, Model model){
-        model.addAttribute("title", "Create Category")
-        model.addAttribute("eventCategory", )
+    @GetMapping("create")
+    public String renderCreateEventCategoryForm(Model model){
+        model.addAttribute("title", "Create Category");
+        model.addAttribute("eventCategory", new EventCategory());
+        return "eventCategories/create";
+    }
+
+    @PostMapping("create")
+    public String processCreateEventCategoryForm(@ModelAttribute @Valid EventCategory newEventCategory, Errors errors, Model model){
+        if(errors.hasErrors()){
+            model.addAttribute("title", "Create Category");
+            model.addAttribute("eventCategory", new EventCategory());
+            return "eventCategories/create";
+        }
+
+        eventCategoryRepository.save(newEventCategory);
+        return "redirect:";
     }
 }
